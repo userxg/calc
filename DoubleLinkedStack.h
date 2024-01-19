6b1ab49d1024cacefb5ecfe74c56b5ad3f5f019b
Fioofa <penavana1@gmail.com>
2024-01-19 19:35:43 +0300

    Появилась поддержка <vec2> и <string> стеков. Исправлена функция AddAtHead(). Можно вызывать длинные функции по которким именам. Подроднее в DoubleLinkedStack.h

--- DoubleLinkedStack.h
+++ DoubleLinkedStack.h
@@ -1,25 +1,100 @@
 ﻿#include <iostream>
+#include <typeinfo>
+#include <string>
 #include <stdint.h>
+
 #include "vec2.h"
+
+#ifndef _Short_Func_Names_
+#define _Short_Func_Names_
+
+#define pop PopTail
+#define popH PopHead
+
+#define add AddItemAtTail
+#define addH AddItemAtHead
+
+#define del DeleteItemFromTail
+#define delH DeleteItemFromHead
+
+#define print PrintStack
+
+#define tmVec2 TestModVec
+#define tmStr TestModString
+
+#endif
+
 /*
-В основном коде НЕ ПОДКЛЮЧАТЬ <iostream> и <stdint.h> и "vec2.h"
+В основном коде НЕ ПОДКЛЮЧАТЬ библиотеки, которые уже включены тут
 Стек реализован для чисел типа vec2 (для комплексных)
 
 Будет доработан до работы со строками
 
+Применять в main:
+Чтобы получить стек из комп.чисел нужно прописать:
+typedef Stack_On_Double_Linked_List<vec2> "название";
+TestModVec() - tmVec() - режим тестирования
+
+Чтобы получить стек из строк нужно прописать:
+typedef Stack_On_Double_Linked_List<string> "название";
+TestModString() - tmStr() - режим тестирования
+
 Функции:
 
-AddItemAtTail() - добавить в конец
-AddItemAtHead() - добавить в начало
+AddItemAtTail() - add() - добавить в конец
+AddItemAtHead() - addH() - добавить в начало
 
-PopTail() - удалить конец
-PopHead() - удалить начало
+PopTail() - pop() - удалить конец
+PopHead() - popH() -удалить начало
 
 DeleteItemFromHead(data) - удалить data. Поиск начинается с конца
 DeleteItemFromHead(data) - удалить data. Поиск начинается с начала
 
 */
+
+/*Test main
+#include "DoubleLinkedStack.h"
+
+typedef Stack_On_Double_Linked_List<vec2> sdll_vec;
+typedef Stack_On_Double_Linked_List<string> sdll_str;
+
+int main() {
+	sdll_vec line;
+	for (int i = 0; i < 10; i++) {
+		vec2 ComplexNum;
+		ComplexNum.real = i;
+
+		line.add(ComplexNum);
+	}
+
+	line.print();
+
+	sdll_str line1;
+
+	string s = "Hello world";
+	string s1 = "Bye world";
+
+	line1.add(s);
+	line1.add(s1);
+	line1.print();
+
+	line1.addH(s1);
+	line1.addH(s);
+	line1.print();
+
+	line1.del(s);
+	line1.print();
+	line1.delH(s1);
+	line1.print();
+
+	return 0;
+}
+
+*/
+
 using namespace std;
+
+template <class DataType>
 
 class Stack_On_Double_Linked_List {
 
@@ -29,29 +104,34 @@ class Stack_On_Double_Linked_List {
 		Node* Prev = NULL;
 		Node* Next = NULL;
 
-		vec2 Num;
+		DataType Val;
 	};
-
-	typedef Stack_On_Double_Linked_List HeadTail;
 
 	Node* Head = NULL;
 	Node* Tail = NULL;
 
-	//Функции
 
+	//Функции
 private:
-	bool EqualData(vec2& data1, vec2& data2) {
-		return (data1.real == data2.real) && (data1.imagine == data2.imagine);
+	bool EqualData(DataType& data1, DataType& data2) {
+
+		if constexpr (is_same<DataType, vec2>::value) {
+			return (data1.real == data2.real) && (data1.imagine == data2.imagine);
+		}
+		else if constexpr (is_same<DataType, string>::value) {
+			return data1 == data2;
+		}
 	}
 
 	bool EmptyStack() {
 		return (Head == NULL) && (Tail == NULL);
 	}
+
 public:
-	void AddItemAtTail(vec2 data) {
+	void AddItemAtTail(DataType data) {
 
 		Node* newNode = new Node();
-		newNode->Num = data;
+		newNode->Val = data;
 
 		if (EmptyStack()) {
 			Head = newNode;
@@ -68,10 +148,10 @@ public:
 
 	}
 
-	void AddItemAtHead(vec2 data) {
+	void AddItemAtHead(DataType data) {
 
 		Node* newNode = new Node();
-		newNode->Num = data;
+		newNode->Val = data;
 
 		if (EmptyStack()) {
 			Head = newNode;
@@ -80,26 +160,12 @@ public:
 			return;
 		}
 
-		Node* prevNode = Head;
+		Node* nextNode = Head;
 
-		prevNode->Prev = newNode;
+		nextNode->Prev = newNode;
+		newNode->Next = nextNode;
 		Head = newNode;
 
-	}
-
-	void PopHead() {
-		if (Head == Tail) {
-			Head = NULL;
-			Tail = NULL;
-			return;
-		}
-
-		Node* HeadNode = Head;
-		Node* nextNode = HeadNode->Next;
-
-		nextNode->Prev = NULL;
-		Head = nextNode;
-		delete HeadNode;
 	}
 
 	void PopTail() {
@@ -120,50 +186,28 @@ public:
 
 	}
 
-	void DeleteItemFromHead(vec2 data) {
-		if (EmptyStack()) {
-			return;
-		}
-
-		Node* node = Head;
-		if (Head == Tail && EqualData(node->Num, data)) {
+	void PopHead() {
+		if (Head == Tail) {
 			Head = NULL;
 			Tail = NULL;
-
-			delete node;
 			return;
 		}
 
-		while (node != NULL) {
-			if (EqualData(node->Num, data)) {
+		Node* HeadNode = Head;
+		Node* nextNode = HeadNode->Next;
 
-				if (node == Head) {
-					PopHead();
-					return;
-				}
-				else if (node == Tail) {
-					PopTail();
-					return;
-				}
-
-				Node* prevNode = node->Prev;
-				Node* nextNode = node->Next;
-
-				prevNode->Next = nextNode;
-				delete node;
-				return;
-			}
-			node = node->Next;
-		}
+		nextNode->Prev = NULL;
+		Head = nextNode;
+		delete HeadNode;
 	}
 
-	void DeleteItemFromTail(vec2 data) {
+	void DeleteItemFromTail(DataType data) {
 		if (EmptyStack()) {
 			return;
 		}
 
 		Node* node = Tail;
-		if (Head == Tail && EqualData(node->Num, data)) {
+		if (Head == Tail && EqualData(node->Val, data)) {
 			Head = NULL;
 			Tail = NULL;
 
@@ -172,7 +216,7 @@ public:
 		}
 
 		while (node != NULL) {
-			if (EqualData(node->Num, data)) {
+			if (EqualData(node->Val, data)) {
 				if (node == Head) {
 					PopHead();
 					return;
@@ -193,10 +237,47 @@ public:
 		}
 	}
 
+	void DeleteItemFromHead(DataType data) {
+		if (EmptyStack()) {
+			return;
+		}
+
+		Node* node = Head;
+		if (Head == Tail && EqualData(node->Val, data)) {
+			Head = NULL;
+			Tail = NULL;
+
+			delete node;
+			return;
+		}
+
+		while (node != NULL) {
+			if (EqualData(node->Val, data)) {
+
+				if (node == Head) {
+					PopHead();
+					return;
+				}
+				else if (node == Tail) {
+					PopTail();
+					return;
+				}
+
+				Node* prevNode = node->Prev;
+				Node* nextNode = node->Next;
+
+				prevNode->Next = nextNode;
+				delete node;
+				return;
+			}
+			node = node->Next;
+		}
+	}
+
 	void PrintStack() {
 
 		if (Head == NULL && Tail == NULL) {
-			cout << "--Current stack is empty--\n\n";
+			cout << "\n\n--Current stack is empty--\n\n";
 			return;
 		}
 
@@ -205,20 +286,29 @@ public:
 
 		cout << "\n\n--Start Stack Log--\n";
 
-		while (node != NULL) {
-			cout << ++c << ". " << node->Num.real << " + " << node->Num.imagine << "i\n";
-			node = node->Next;
+		if constexpr (is_same<DataType, vec2>::value) {
+			while (node != NULL) {
+				cout << ++c << ". " << node->Val.real << " + " << node->Val.imagine << "i\n";
+				node = node->Next;
+			}
+		}else if constexpr(is_same<DataType, string>::value) {
+			while (node != NULL) {
+				cout << ++c << ". " << node->Val << "\n";
+				node = node->Next;
+			}
 		}
 
 		cout << "--End Stack Log--\n\n";
 
 	}
 
-	void TestMod() {
+	void TestModVec() {
 
 		cout << "--Test mode of Node by Voskoboynik--\n$.Stop\nt.Add at tail\nh.Add at head.\nT.Pop tail\nH.Pop head\nd.Delete from tail\nD.Delete from head\n\n";
 		char UserInput = ' ';
+
 		vec2 data;
+
 		while (UserInput != '$') {
 			cin >> UserInput;
 			switch (UserInput) {
@@ -229,10 +319,12 @@ public:
 				PrintStack();
 				break;
 			case 't':
+
 				cin >> data.real;
 				AddItemAtTail(data);
 				break;
 			case 'h':
+
 				cin >> data.real;
 				AddItemAtHead(data);
 				break;
@@ -243,15 +335,67 @@ public:
 				PopHead();
 				break;
 			case 'd':
+
 				cin >> data.real;
-				DeleteItemFromTail( data);
+				DeleteItemFromTail(data);
 				break;
 			case 'D':
+
 				cin >> data.real;
-				DeleteItemFromHead( data);
+				DeleteItemFromHead(data);
 				break;
 			default:
 				cout << "Wrong commend\n";
+				break;
+			}
+		}
+	}
+
+	void TestModString() {
+
+		cout << "--Test mode of Node by Voskoboynik--\n$.Stop\nt.Add at tail\nh.Add at head.\nT.Pop tail\nH.Pop head\nd.Delete from tail\nD.Delete from head\n\n";
+		char UserInput = ' ';
+
+		string data;
+
+		while (UserInput != '$') {
+			cin >> UserInput;
+			switch (UserInput) {
+			case '$':
+				cout << "--Test mod is off--";
+				break;
+			case 'p':
+				PrintStack();
+				break;
+			case 't':
+
+				cin >> data;
+				AddItemAtTail(data);
+				break;
+			case 'h':
+
+				cin >> data;
+				AddItemAtHead(data);
+				break;
+			case 'T':
+				PopTail();
+				break;
+			case 'H':
+				PopHead();
+				break;
+			case 'd':
+
+				cin >> data;
+				DeleteItemFromTail(data);
+				break;
+			case 'D':
+
+				cin >> data;
+				DeleteItemFromHead(data);
+				break;
+			default:
+				cout << "Wrong commend\n";
+				break;
 			}
 		}
 	}
